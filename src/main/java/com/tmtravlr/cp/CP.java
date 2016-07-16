4555088b2fb81674f6e9025f75eb0dad3dcffa9f
Choonster TheMage <Choonster.2010@gmail.com>
2016-07-17 03:00:06 +1000

    Clean up code
    - Fix ColourfulData NBT reading/writing
    - Store a single instance of ColourfulData while the server is running
    -- Adapted from RFTools by McJty
    - Rename CPLocations to CPLocation
    - Replace the x/y/z fields of CPLocation with a BlockPos
    - Add missing generics
    - Replace most copy-pasted BlockPos facing offset code with EnumFacing
    loops
    - Fix incorrect override method signatures
    - Add missing @Override annotations
    - Remove fields that shouldn't exist

--- src/main/java/com/tmtravlr/cp/CP.java
+++ src/main/java/com/tmtravlr/cp/CP.java
@@ -3,7 +3,7 @@ package com.tmtravlr.cp;
 import com.tmtravlr.cp.init.ColourfulFluids;
 import com.tmtravlr.cp.init.ColourfulItems;
 import com.tmtravlr.cp.proxy.ICommonProxy;
-
+import com.tmtravlr.cp.worldsavedata.ColourfulData;
 import net.minecraftforge.fluids.FluidRegistry;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.Mod.EventHandler;
@@ -12,35 +12,44 @@ import net.minecraftforge.fml.common.SidedProxy;
 import net.minecraftforge.fml.common.event.FMLInitializationEvent;
 import net.minecraftforge.fml.common.event.FMLPostInitializationEvent;
 import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
+import net.minecraftforge.fml.common.event.FMLServerStoppedEvent;
 
 @Mod(modid = CPLib.MODID, name = CPLib.NAME, version = CPLib.VERSION/*, acceptedMinecraftVersions = ""*/)
 public class CP {
-	
-	{
+
+	static {
 		FluidRegistry.enableUniversalBucket();
 	}
+
 	@Instance
 	public static CP cpInstance;
-	
+
 	@SidedProxy(clientSide = CPLib.CLIENT_PROXY_CLASS, serverSide = CPLib.SERVER_PROXY_CLASS)
 	public static ICommonProxy proxy;
-	
+
 	@EventHandler
-	public void preInit(FMLPreInitializationEvent e){
+	public void preInit(FMLPreInitializationEvent e) {
 		System.out.println(CPLib.MODID + " preinitialized");
 		ColourfulItems.init();
 		ColourfulItems.register();
 		ColourfulFluids.dummyRegister();
 	}
-	
+
 	@EventHandler
-	public void init(FMLInitializationEvent e){
+	public void init(FMLInitializationEvent e) {
 		System.out.println(CPLib.MODID + " initialized");
 		proxy.init();
 	}
+
 	@EventHandler
-	public void postInit(FMLPostInitializationEvent e){
+	public void postInit(FMLPostInitializationEvent e) {
 		System.out.println(CPLib.MODID + " postinitialized");
 	}
-	
+
+	@EventHandler
+	public void serverStopped(FMLServerStoppedEvent e) {
+		// Clear the ColourfulData instance when the server stops
+		ColourfulData.clearInstance();
+	}
+
 }
