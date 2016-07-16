3a70fb756bec3984a4c620457df606277734edcc
Choonster TheMage <Choonster.2010@gmail.com>
2016-07-17 01:03:13 +1000

    Add package-info files with default MCP annotations

---
+++ src/main/java/com/tmtravlr/cp/proxy/package-info.java
@@ -0,0 +1,7 @@
+@ParametersAreNonnullByDefault
+@MethodsReturnNonnullByDefault
+package com.tmtravlr.cp.proxy;
+
+import mcp.MethodsReturnNonnullByDefault;
+
+import javax.annotation.ParametersAreNonnullByDefault;
